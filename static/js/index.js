022364bce8af37782c19f441d830496e8bc1ba1a
rhyolight <matt@numenta.org>
2019-02-08 12:23:31 -0800

    Progress bars

--- static/js/index.js
+++ static/js/index.js
@@ -5,6 +5,7 @@ $(function() {
     let $canvasBag = $('#canvas-bag')
     let $noiseSlider = $('#noise')
     let $noiseLabel = $('#noise-label')
+    let $spinner = $('.spinner-border')
 
     // Functions
 
@@ -16,8 +17,18 @@ $(function() {
       $.getJSON("/_mnist/" + batch + "/" + noise, cb);
     }
 
-    function highlightWinner() {
+    /* From http://stackoverflow.com/questions/7128675/from-green-to-red-color-depend-on-percentage */
+    function getGreenToRed(percent){
+        var r, g;
+        percent = 100 - percent;
+        r = percent < 50 ? 255 : Math.floor(255-(percent*2-100)*255/100);
+        g = percent > 50 ? 255 : Math.floor((percent*2)*255/100);
+        return rgbToHex(r, g, 0);
+    }
 
+    /* From http://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb */
+    function rgbToHex(r, g, b) {
+        return ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
     }
 
     function renderMnist(resp) {
@@ -34,20 +45,25 @@ $(function() {
             let results = resp.classifications[c]
             let accuracy = Math.round(results.accuracy * 100)
             $('#' + c + '-score').html(accuracy + ' %')
-            $('#' + c + '-progress').css('width', accuracy + '%')
+            $('#' + c + '-progress').css({
+              'width': accuracy + '%',
+              'background-color': '#' + getGreenToRed(100-accuracy),
+            })
         })
+        $spinner.hide()
     }
 
     // Program start
     let width = 28
     let height = 28
     let batch = 400
-    let startingNoise = 15
+    let startingNoise = 0
     let communicating = false
 
     $noiseLabel.html(startingNoise)
     $noiseSlider.val(startingNoise)
     $noiseSlider.change((evt) => {
+        $spinner.show()
         getMnist(batch, $noiseSlider.val(), renderMnist)
     })
 
