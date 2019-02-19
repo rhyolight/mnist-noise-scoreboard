917bcdfebc196fa3a8f42ee67e1e36e961730332
rhyolight <matt@numenta.org>
2019-02-19 11:38:57 -0800

    Made room for Google Speech.

--- server.py
+++ server.py
@@ -7,28 +7,36 @@ Usage::
 # from __future__ import (absolute_import, division,
 #                         print_function, unicode_literals)
 
-from flask import Flask
 from flask_restful import Api, Resource, reqparse
-from flask import Flask, request, send_from_directory
+from flask import Flask, send_from_directory
 
-import numpy as np
-import torch
 import torch.nn.functional as F
 from torchvision import datasets, transforms
 
+from pytorch.speech_commands_dataset import (
+    SpeechCommandsDataset, BackgroundNoiseDataset
+)
 from image_transforms import *
 
 # ML models
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 models = {}
-models['denseCNN1'] = torch.load('models/denseCNN1.pt', map_location=device)
-models['sparseCNN1'] = torch.load('models/sparseCNN1.pt', map_location=device)
+models['mnist'] = {}
+print('Loading MNIST models...')
+models['mnist']['denseCNN1'] = torch.load('models/mnist/denseCNN1.pt', map_location=device)
+models['mnist']['sparseCNN1'] = torch.load('models/mnist/sparseCNN1.pt', map_location=device)
+print('Loading Speech models...')
+models['speech'] = {}
+models['speech']['denseCNN1'] = torch.load('models/speech/denseCNN2_c1_out_channels64_64k1000n1000.pt',
+                                           map_location=device)
+models['speech']['sparseCNN1'] = torch.load('models/speech/sparseCNN2_n1000.pt', map_location=device)
 
-def getClassificationResults(data, target):
+
+def getClassificationResults(data, type, target):
     result = {}
     with torch.no_grad():
-        for name in models:
-            model = models[name]
+        for name in models[type]:
+            model = models[type][name]
             data, target = data.to(device), target.to(device)
             output = model(data)
             test_loss = F.nll_loss(output, target, reduction='sum').item()
@@ -39,23 +47,35 @@ def getClassificationResults(data, target):
             result[name]["classifications"] = pred.cpu().numpy().tolist()
     return result
 
+
 # Web App:
 app = Flask(__name__)
 api = Api(app)
 parser = reqparse.RequestParser()
 
+
 @app.route('/')
 def index():
     return send_from_directory('static', 'index.html')
+
+
+@app.route('/mnist.html')
+def mnist_static():
+    return send_from_directory('static', 'mnist.html')
+
+
+@app.route('/speech.html')
+def speech_static():
+    return send_from_directory('static', 'speech.html')
+
 
 @app.route('/css/styles.css')
 def styles():
     return send_from_directory('static/css', 'styles.css')
 
-class Mnist(Resource):
 
-    def post(self):
-        args = parser.parse_args()
+class Mnist(Resource):
+    NAME = "mnist"
 
     def get(self, batch, xform, xformStrength):
         if xform == "noise":
@@ -77,26 +97,115 @@ class Mnist(Resource):
 
         # Only doing one xform at a time, but could apply many.
 
-        dataset = datasets.MNIST("mnist",
-            train=False,
-            download=True,
-            transform=transforms.Compose(
-                [
-                    transforms.ToTensor(),
-                    imageTransform,
-                    transforms.Normalize((0.1307,), (0.3081,))
-                ]
-            ),
-        )
+        dataset = datasets.MNIST('data/' + self.NAME,
+                                 train=False,
+                                 download=True,
+                                 transform=transforms.Compose(
+                                     [
+                                         transforms.ToTensor(),
+                                         imageTransform,
+                                         transforms.Normalize((0.1307,), (0.3081,))
+                                     ]
+                                 ),
+                                 )
         dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch, shuffle=True)
         batch_idx, (example_data, example_targets) = next(enumerate(dataloader))
 
         response = {
             "data": example_data.data.cpu().numpy().tolist(),
             "targets": example_targets.data.cpu().numpy().tolist(),
-            "classifications": getClassificationResults(example_data, example_targets),
+            "classifications": getClassificationResults(example_data, self.NAME, example_targets),
         }
         return response, 200
 
+
 api.add_resource(Mnist, "/_mnist/<int:batch>/<string:xform>/<int:xformStrength>")
+
+
+class Speech(Resource):
+    NAME = "speech"
+
+    def post(self):
+        args = parser.parse_args()
+
+    def get(self, batch, xform, xformStrength):
+
+        trainDataDir = os.path.join("data", self.NAME, "train")
+
+        # Create transforms
+        # if xform == "noise":
+        #     imageTransform = RandomNoise(xformStrength / 100.)
+        # elif xform == "grid":
+        #     imageTransform = Grid(size=xformStrength)
+        # elif xform == "wipe":
+        #     imageTransform = Wipe(percent=xformStrength / 100.)
+        # elif xform == "invert":
+        #     imageTransform = Invert()
+        # elif xform == "swirl":
+        #     imageTransform = Swirl(level=xformStrength / 100.)
+        # elif xform == "washout":
+        #     imageTransform = Washout(level=xformStrength / 100.)
+        # elif xform == "rotate":
+        #     imageTransform = Rotate(degrees=xformStrength)
+        # else:
+        #     imageTransform = transforms.ToTensor
+
+        # dataAugmentationTransform = transforms.Compose([
+        #     ChangeAmplitude(),
+        #     ChangeSpeedAndPitchAudio(),
+        #     FixAudioLength(),
+        #     ToSTFT(),
+        #     StretchAudioOnSTFT(),
+        #     TimeshiftAudioOnSTFT(),
+        #     FixSTFTDimension(),
+        # ])
+        #
+        # featureTransform = transforms.Compose(
+        #     [
+        #         ToMelSpectrogramFromSTFT(n_mels=n_mels),
+        #         DeleteSTFT(),
+        #         ToTensor('mel_spectrogram', 'input')
+        #     ])
+        #
+        # trainDataset = SpeechCommandsDataset(
+        #     trainDataDir,
+        #     transforms.Compose([
+        #         LoadAudio(),
+        #         dataAugmentationTransform,
+        #         # add_bg_noise,               # Uncomment to allow adding BG noise
+        #         # during training
+        #         featureTransform
+        #     ]))
+
+
+        # Create dataset and loader
+        # dataset = datasets.MNIST(self.NAME,
+        #     train=False,
+        #     download=True,
+        #     transform=transforms.Compose(
+        #         [
+        #             transforms.ToTensor(),
+        #             imageTransform,
+        #             transforms.Normalize((0.1307,), (0.3081,))
+        #         ]
+        #     ),
+        # )
+        # dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch, shuffle=True)
+
+        # Run One Batch
+        # batch_idx, (example_data, example_targets) = next(enumerate(dataloader))
+
+        # Construct and emit response
+        # response = {
+        #     "data": example_data.data.cpu().numpy().tolist(),
+        #     "targets": example_targets.data.cpu().numpy().tolist(),
+        #     "classifications": getClassificationResults(example_data, self.NAME, example_targets),
+        # }
+        # return response, 200
+
+        return 404
+
+
+api.add_resource(Speech, "/_speech/<int:batch>/<string:xform>/<int:xformStrength>")
+
 app.run(debug=True)
